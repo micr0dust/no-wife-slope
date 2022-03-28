7a7a73f5221199d61b13c19cc6a07e1029693382
micr0dust <wuilliamolgo104286@gmail.com>
2022-03-28 09:40:52 +0800

    useless modify

--- js/const.js
+++ js/const.js
@@ -1,11 +1,10 @@
-let pre_screenW=window.screen.width;
-let pre_screenH=window.screen.height;
+let pre_screenW = window.screen.width;
+let pre_screenH = window.screen.height;
 let testVal;
-if(pre_screenW<pre_screenH){
-    testVal=pre_screenW;
-    pre_screenW=pre_screenH;
-    pre_screenH=testVal;
+if (pre_screenW < pre_screenH) {
+    testVal = pre_screenW;
+    pre_screenW = pre_screenH;
+    pre_screenH = testVal;
 }
 const screenW = pre_screenW;
 const screenH = pre_screenH;
-const speed = 5;
